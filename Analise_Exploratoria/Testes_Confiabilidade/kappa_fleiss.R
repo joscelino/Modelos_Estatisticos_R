577143d1912d8c293afda3652c727555b9973bee
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-05-20 08:29:55 -0300

    #4 - Modelo para teste kappa Fleiss

--- Analise_Exploratoria/Testes_Confiabilidade/kappa_fleiss.R
+++ Analise_Exploratoria/Testes_Confiabilidade/kappa_fleiss.R
@@ -1,18 +1,17 @@
 # 1. Pacotes necessarios
 library(dplyr)
-library(rel)
+library(DescTools)
 library(irr)
 library(tibble)
 
 
 # 2.  Importacao dos dados
-dados <- read.csv2("D:/Projetos_em_R/Modelos_Estatisticos/Analise_Exploratoria/Dados/kappa_cohen.csv",
+dados <- read.csv2("D:/Projetos_em_R/Modelos_Estatisticos/Analise_Exploratoria/Dados/dados_fleiss.csv",
                    stringsAsFactors = TRUE) 
 dados <- tibble::as_tibble(dados)
 dplyr::glimpse(dados)
 
-
-# 3. Calculo do Kappa de Cohen (2 observadores)
+# 3. Calculo do Kappa
 # H0: kappa = 0 | H1: kappa != 0
 # Interpretacao do kappa (McHugh, 2012)
 # 0.0 - 0.2 - Nula
@@ -21,14 +20,10 @@ dplyr::glimpse(dados)
 # 0.6 - 0.79 - Moderada
 # 0.8 - 0.9 - Forte
 # acima de 0.9 - Perfeita
-irr::kappa2(dados[-1])
+irr::kappam.fleiss(dados[2: 4], detail = TRUE)
 
 # 4. Calculo do IC 95%
-rel::ckap(data = dados[-1], 
-          weight = "unweighted", 
-          std.err = "Cohen", 
-          conf.level = 0.95)
+DescTools::KappaM(dados[2: 4], method = "Fleiss", conf.level = 0.95)
 
-# 5. Calculo da concordancia
-irr::agree(dados[-1])
-
+# 4. Calculo da concordancia
+irr::agree(dados[2: 4])
